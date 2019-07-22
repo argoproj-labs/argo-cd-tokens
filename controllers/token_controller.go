9c5f174b1266619561fd26410f3d54e061134512
Dillen Padhiar <dpadhiar@ucsd.edu>
2019-07-22 16:24:57 -0700

    Added Get and Post requests to obtain token from ArgoCD

--- controllers/token_controller.go
+++ controllers/token_controller.go
@@ -16,8 +16,14 @@ limitations under the License.
 package controllers
 
 import (
+	"bytes"
 	"context"
+	"crypto/tls"
 	"fmt"
+
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
@@ -63,6 +69,73 @@ func (p *patchSecretKey) Data(obj runtime.Object) ([]byte, error) {
 	return []byte(patch), nil
 }
 
+// PostRequest used for RequestPayload
+type PostRequest struct {
+	ExpiresIn int
+	Project   string
+	Role      string
+}
+
+// AppProject provides a logical grouping of applications, providing controls for:
+// * where the apps may deploy to (cluster whitelist)
+// * what may be deployed (repository whitelist, resource whitelist/blacklist)
+// * who can access these applications (roles, OIDC group claims bindings)
+// * and what they can do (RBAC policies)
+// * automation access to these roles (JWT tokens)
+// +genclient
+// +genclient:noStatus
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:resource:path=appprojects,shortName=appproj;appprojs
+type AppProject struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata" protobuf:"bytes,1,opt,name=metadata"`
+	Spec              AppProjectSpec `json:"spec" protobuf:"bytes,2,opt,name=spec"`
+}
+
+// AppProjectSpec is the specification of an AppProject
+type AppProjectSpec struct {
+	// SourceRepos contains list of git repository URLs which can be used for deployment
+	SourceRepos []string `json:"sourceRepos,omitempty" protobuf:"bytes,1,name=sourceRepos"`
+	// Destinations contains list of destinations available for deployment
+	Destinations []ApplicationDestination `json:"destinations,omitempty" protobuf:"bytes,2,name=destination"`
+	// Description contains optional project description
+	Description string `json:"description,omitempty" protobuf:"bytes,3,opt,name=description"`
+	// Roles are user defined RBAC roles associated with this project
+	Roles []ProjectRole `json:"roles,omitempty" protobuf:"bytes,4,rep,name=roles"`
+	// ClusterResourceWhitelist contains list of whitelisted cluster level resources
+	ClusterResourceWhitelist []metav1.GroupKind `json:"clusterResourceWhitelist,omitempty" protobuf:"bytes,5,opt,name=clusterResourceWhitelist"`
+	// NamespaceResourceBlacklist contains list of blacklisted namespace level resources
+	NamespaceResourceBlacklist []metav1.GroupKind `json:"namespaceResourceBlacklist,omitempty" protobuf:"bytes,6,opt,name=namespaceResourceBlacklist"`
+}
+
+// ApplicationDestination contains deployment destination information
+type ApplicationDestination struct {
+	// Server overrides the environment server value in the ksonnet app.yaml
+	Server string `json:"server,omitempty" protobuf:"bytes,1,opt,name=server"`
+	// Namespace overrides the environment namespace value in the ksonnet app.yaml
+	Namespace string `json:"namespace,omitempty" protobuf:"bytes,2,opt,name=namespace"`
+}
+
+// ProjectRole represents a role that has access to a project
+type ProjectRole struct {
+	// Name is a name for this role
+	Name string `json:"name" protobuf:"bytes,1,opt,name=name"`
+	// Description is a description of the role
+	Description string `json:"description,omitempty" protobuf:"bytes,2,opt,name=description"`
+	// Policies Stores a list of casbin formated strings that define access policies for the role in the project
+	Policies []string `json:"policies,omitempty" protobuf:"bytes,3,rep,name=policies"`
+	// JWTTokens are a list of generated JWT tokens bound to this role
+	JWTTokens []JWTToken `json:"jwtTokens,omitempty" protobuf:"bytes,4,rep,name=jwtTokens"`
+	// Groups are a list of OIDC group claims bound to this role
+	Groups []string `json:"groups,omitempty" protobuf:"bytes,5,rep,name=groups"`
+}
+
+// JWTToken holds the issuedAt and expiresAt values of a token
+type JWTToken struct {
+	IssuedAt  int64 `json:"iat" protobuf:"int64,1,opt,name=iat"`
+	ExpiresAt int64 `json:"exp,omitempty" protobuf:"int64,2,opt,name=exp"`
+}
+
 // Reconcile s
 // +kubebuilder:rbac:groups=argoprojlabs.argoproj-labs.io,resources=tokens,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=argoprojlabs.argoproj-labs.io,resources=tokens/status,verbs=get;update;patch
@@ -79,55 +152,102 @@ func (r *TokenReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		return ctrl.Result{}, nil
 	}
 
-	// conn := argocdclient.NewClientOrDie(clientOpts)
-	// TODO: projIf, err :=
+	// https://www.socketloop.com/tutorials/golang-disable-security-check-for-http-ssl-with-bad-or-expired-certificate
+	transCfg := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true}, // ignore expired SSL certificates
+	}
+	client := &http.Client{Transport: transCfg}
 
-	// token, err := projIf.CreateToken(context.Background(), &projectpkg.ProjectTokenCreateRequest{Project: projName, Role: roleName, ExpiresIn: int64(duration.Seconds())})
+	request, err := http.NewRequest("GET", "https://10.107.242.190/api/v1/projects/default", nil)
 
-	// tokenString := "practice string"
+	loginCookie := http.Cookie{
+		Name:     "argocd.token",
+		Value:    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NjI5MDI3MjAsImlzcyI6ImFyZ29jZCIsIm5iZiI6MTU2MjkwMjcyMCwic3ViIjoiYWRtaW4ifQ.j0tOpDRSgHesKZw8Ghkzqa_yaRi5sDzqQw24a78AbPs",
+		Path:     "/",
+		MaxAge:   60 * 60,
+		HttpOnly: true,
+	}
 
-	tokenStr := "let's try updating now"
-	secret, _ := r.createSecret(ctx, tokenStr, logCtx, token)
+	request.AddCookie(&loginCookie)
 
-	secretMsg := fmt.Sprintf("%s exists", secret.ObjectMeta.Name)
-	dataMsg := fmt.Sprintf("%s exists", secret.Data)
-	namespaceMsg := fmt.Sprintf("%s is the ns", secret.ObjectMeta.Namespace)
-	logCtx.Info(secretMsg)
-	logCtx.Info(dataMsg)
-	logCtx.Info(namespaceMsg)
+	response, err := client.Do(request)
+	if err != nil {
+		logCtx.Info(err.Error())
+		return ctrl.Result{}, nil
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		logCtx.Info(err.Error())
+		return ctrl.Result{}, nil
+	}
+
+	var project AppProject
+
+	err = json.Unmarshal(body, &project)
+	if err != nil {
+		logCtx.Info(err.Error())
+		return ctrl.Result{}, nil
+	}
+
+	postReq := PostRequest{
+		ExpiresIn: 10,
+		Project:   token.Spec.Project,
+		Role:      token.Spec.Role,
+	}
+
+	bytePostReq, err := json.Marshal(postReq)
+	if err != nil {
+		logCtx.Info(err.Error())
+		return ctrl.Result{}, nil
+	}
+
+	request, err = http.NewRequest("POST", "https://10.107.242.190/api/v1/projects/default/roles/TestRole/token", bytes.NewBuffer(bytePostReq))
+	if err != nil {
+		logCtx.Info(err.Error())
+		return ctrl.Result{}, nil
+	}
+	request.Header.Set("Content-Type", "application/json")
+	request.AddCookie(&loginCookie)
+
+	response, err = client.Do(request)
+	if err != nil {
+		logCtx.Info(err.Error())
+		return ctrl.Result{}, nil
+	}
+
+	defer response.Body.Close()
+
+	body, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		logCtx.Info(err.Error())
+		return ctrl.Result{}, nil
+	}
+
+	fmt.Println(string(body))
+
+	tokenStr := "this will come from ArgoCD eventually"
+	secret, wasPatched, err := r.createSecret(ctx, tokenStr, logCtx, token)
+	if err != nil {
+		logCtx.Info(err.Error())
+		return ctrl.Result{}, nil
+	}
+
+	if wasPatched {
+		secretMsg := fmt.Sprintf("Secret %s updated!", secret.ObjectMeta.Name)
+		logCtx.Info(secretMsg)
+	} else {
+		secretMsg := fmt.Sprintf("Secret %s created!", secret.ObjectMeta.Name)
+		logCtx.Info(secretMsg)
+	}
 
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager s
 func (r *TokenReconciler) SetupWithManager(mgr ctrl.Manager) error {
-
-	/* if err := mgr.GetFieldIndexer().IndexField(&corev1.Secret{}, "metadata.name", func(rawObj runtime.Object) []string {
-		// grab the object
-		ctx := context.Background()
-		var tknList argoprojlabsv1.TokenList
-		//fmt.Println("Function was entered")
-
-		secret := rawObj.(*corev1.Secret)
-		tknNames := make([]string, 0)
-
-		err := r.List(ctx, &tknList)
-		if err != nil {
-			return tknNames
-		}
-
-		for _, token := range tknList.Items {
-			if secret.Name == token.Spec.SecretRef.Name {
-				//s := fmt.Sprintf("%s", token.Name)
-				fmt.Println(secret.Name)
-				tknNames = append(tknNames, secret.Name)
-			}
-		}
-
-		return tknNames
-	}); err != nil {
-		return err
-	} */
 
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&argoprojlabsv1.Token{}).
@@ -137,7 +257,6 @@ func (r *TokenReconciler) SetupWithManager(mgr ctrl.Manager) error {
 
 					ctx := context.Background()
 					var tknList argoprojlabsv1.TokenList
-					//tknNames := make([]string, 0)
 					tknMatches := make([]argoprojlabsv1.Token, 0)
 
 					err := r.List(ctx, &tknList)
@@ -148,7 +267,6 @@ func (r *TokenReconciler) SetupWithManager(mgr ctrl.Manager) error {
 					for _, token := range tknList.Items {
 						if a.Meta.GetName() == token.Spec.SecretRef.Name {
 							fmt.Println(token.Name)
-							//tknNames = append(tknNames, token.Name)
 							tknMatches = append(tknMatches, token)
 						}
 					}
@@ -170,7 +288,7 @@ func (r *TokenReconciler) SetupWithManager(mgr ctrl.Manager) error {
 }
 
 // A helper function to create Secrets from strings
-func (r *TokenReconciler) createSecret(ctx context.Context, tknStr string, logCtx logr.Logger, token argoprojlabsv1.Token) (*corev1.Secret, error) {
+func (r *TokenReconciler) createSecret(ctx context.Context, tknStr string, logCtx logr.Logger, token argoprojlabsv1.Token) (*corev1.Secret, bool, error) {
 
 	namespaceName := types.NamespacedName{
 		Name:      token.Spec.SecretRef.Name,
@@ -189,9 +307,9 @@ func (r *TokenReconciler) createSecret(ctx context.Context, tknStr string, logCt
 		err = r.Patch(ctx, &secret, patch)
 		if err != nil {
 			logCtx.Info(err.Error())
-			return nil, err
+			return nil, false, err
 		}
-		return &secret, nil
+		return &secret, true, nil
 	}
 
 	secret = corev1.Secret{
@@ -206,7 +324,12 @@ func (r *TokenReconciler) createSecret(ctx context.Context, tknStr string, logCt
 	err = r.Create(ctx, &secret)
 	if err != nil {
 		logCtx.Info(err.Error())
-		return nil, err
+		return nil, false, err
 	}
-	return &secret, nil
+	return &secret, false, nil
 }
+
+/* func fetchToken(url, loginToken string) (tokenStr string, err error) {
+
+	resp, err := http.Get("http://example.com/")
+} */
--- go.mod
+++ go.mod
@@ -3,32 +3,44 @@ module github.com/dpadhiar/argo-cd-tokens
 go 1.12
 
 require (
-	github.com/Masterminds/semver v1.4.2 // indirect
-	github.com/argoproj/argo-cd v1.0.2 // indirect
-	github.com/coreos/go-oidc v2.0.0+incompatible // indirect
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/Masterminds/semver v1.4.2
+	github.com/argoproj/argo-cd v1.0.2
+	github.com/argoproj/pkg v0.0.0-20190708182346-fb13aebbef1c
+	github.com/coreos/go-oidc v2.0.0+incompatible
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/emicklei/go-restful v2.9.6+incompatible // indirect
+	github.com/evanphx/json-patch v4.1.0+incompatible
+	github.com/ghodss/yaml v1.0.0
 	github.com/go-logr/logr v0.1.0
-	github.com/go-openapi/spec v0.19.2 // indirect
-	github.com/go-redis/cache v6.4.0+incompatible // indirect
-	github.com/go-redis/redis v6.15.2+incompatible // indirect
-	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0 // indirect
-	github.com/grpc-ecosystem/grpc-gateway v1.9.4 // indirect
-	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/go-openapi/spec v0.19.2
+	github.com/go-redis/cache v6.4.0+incompatible
+	github.com/go-redis/redis v6.15.2+incompatible
+	github.com/gobwas/glob v0.2.3
+	github.com/gogo/protobuf v1.2.1
+	github.com/golang/protobuf v1.2.0
+	github.com/grpc-ecosystem/go-grpc-middleware v1.0.0
+	github.com/grpc-ecosystem/grpc-gateway v1.9.4
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51
 	github.com/onsi/ginkgo v1.8.0
 	github.com/onsi/gomega v1.5.0
-	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
-	github.com/sirupsen/logrus v1.4.2 // indirect
-	github.com/vmihailenco/msgpack v4.0.4+incompatible // indirect
+	github.com/sirupsen/logrus v1.4.2
+	github.com/spf13/cobra v0.0.3
+	github.com/stretchr/testify v1.3.0
+	github.com/vmihailenco/msgpack v4.0.4+incompatible
+	golang.org/x/crypto v0.0.0-20190611184440-5c40567a22f8
 	golang.org/x/net v0.0.0-20190613194153-d28f0bde5980
-	google.golang.org/grpc v1.22.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+	google.golang.org/genproto v0.0.0-20180817151627-c66870c02cf8
+	google.golang.org/grpc v1.22.0
 	gopkg.in/square/go-jose.v2 v2.3.1 // indirect
-	gopkg.in/src-d/go-git.v4 v4.12.0 // indirect
+	gopkg.in/src-d/go-git.v4 v4.12.0
+	gopkg.in/yaml.v2 v2.2.2
 	k8s.io/api v0.0.0-20190409021203-6e4e0e4f393b
 	k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
 	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
+	k8s.io/kube-openapi v0.0.0-20180731170545-e3762e86a74c
 	sigs.k8s.io/controller-runtime v0.2.0-beta.2
 	sigs.k8s.io/controller-tools v0.2.0-beta.2 // indirect
 )
